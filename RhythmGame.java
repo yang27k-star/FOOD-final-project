8476f125218258bc68f690e3221f8d3d5937cbfa
Kevin <cruiseshipky@gmail.com>
2025-12-01 14:39:27 -0500

    Co-authored-by: MaxWeiGithub <MaxWeiGithub@users.noreply.github.com>

--- RhythmGame.java
+++ RhythmGame.java
@@ -11,16 +11,7 @@ import javafx.util.Duration;
 import java.util.ArrayList;
 import javafx.scene.control.Label;
 
-/**
- * A simple JavaFX animation examples. Animates a Circle's X property by
- * translating (moving) it 200 points over 10 seconds.
- */
-
-
-//The story begins with exposition of the world: the player was recently displaced by something. They don't have money, but the town square has an old piano. The player has vague memories of learning basics during childhood, and they begin practicing. at this point, start either a tutorial or an easy song. next, they keep practicing and meet a teacher. they can choose to learn under the teacher or practice themselves. if they practice themselves, the next song is harder(not sure how though,this might not be mandatory as well). depending on their score they gain money. if they get enough money, they can move on to playing in a bar. If they picked a teacher the teacher has no more to teach and the bar songs get harder to play. if not, the songs are easier to play because the player grasped the essence of piano themselves. when they play enough and earn enough, they can play at the town hall for the mayor who praises the player if they do well and taxes them if they dont(they go back to the bar). 
-
-
-public class Main extends Application {
+public class RhythmGame extends Application {
     private static int numberOfNotes = 100;
     private static ArrayList<Note> notes = new ArrayList<>();
     private static KeyFrame[] keyFrames = new KeyFrame[numberOfNotes];
