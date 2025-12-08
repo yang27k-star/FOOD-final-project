41576b707d71c1cde03d3b0f399ecee526e6cc65
MaxWeiGithub <maxduwei@gmail.com>
2025-12-08 02:35:18 -0500

    Exposition complete(more or less) and made a transition into the first game section

--- src/DialogueScreen.java
+++ src/DialogueScreen.java
@@ -13,27 +13,41 @@ public class DialogueScreen extends VBox {
     public DialogueScreen(SceneManager sceneManager) {
         setSpacing(10);
         setAlignment(Pos.CENTER);
-        Text text = new Text("This is a text.");
-        VBox vBox = new VBox(text);
+    
 
-        
-        Scene scene = new Scene(new VBox(text), 300, 250);
-        
-
-        text.setText("This is the text to display");
-        text.setX(50);
-        text.setY(25);
-        Label gameLabel = new Label("Game Screen Placeholder");
-
+        Label narration = new Label("You see flowers everywhere, stretching past welcoming mountains and lovely valley alike.");
         Button startButton = new Button("Wake up.");
+        getChildren().addAll(startButton, narration);
         startButton.setOnAction(e -> {
-            System.out.println("stuff");
+            getChildren().remove(startButton);
+            narration.setText("Oh... That was a dream. You find yourself in your same old bed, the same, smelly clothes, and the same 12-cent-per-day closet you sleep in.");
+            getChildren().add(narration);
+            Button continueButton = new Button("Continue");
+            getChildren().add(continueButton);
+            continueButton.setOnAction(ee -> {
+                getChildren().removeAll(continueButton,narration);
+                narration.setText("Searching for your usual donation at the mall entrance, you suddenly notice a decrepit upright piano hidden conspicuously near you. Suddenly, memories of your past appear: your grandmother used to sit you on her lap and have you play 'Hot Cross Buns'");
+                getChildren().add(narration);
+                Button playPiano = new Button("Play the piano");
+                Button leavePiano = new Button("Walk away");
+                getChildren().addAll(playPiano, leavePiano);
+                playPiano.setOnAction(eee -> {
+                    getChildren().removeAll(playPiano,narration);
+                    narration.setText("What is this feeling... You feel... INSPIRED");
+                    getChildren().addAll(narration,continueButton);
+                    continueButton.setOnAction(eeee -> {
+                        getChildren().removeAll(continueButton,narration);
+                        sceneManager.createRhythmGameScreen();
+                        sceneManager.showRhythmGame();
+                    });
+                });
+            });
             
         });
         //exitButton.setOnAction(e -> System.exit(0));
 
         // Add controls to this layout
-        getChildren().addAll(startButton/*, exitButton*/);
+        //getChildren().addAll(startButton/*, exitButton*/);
 
         //Button exitButton = new Button("Exit");
     }
